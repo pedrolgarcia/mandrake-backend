7713d38391694fe28011ab8c36554eee96f852c6
Pedro Lopes <pedrolopes.info@gmail.com>
2021-05-27 01:40:46 -0300

    feat: global exception handler

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,27 +1,16 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class AuthController {
-    public async login({ request, auth, response }: HttpContextContract) {
+    public async login({ request, auth }: HttpContextContract) {
         const username = request.input('username')
         const password = request.input('password')
     
-        try {
-            const token = await auth.use('api').attempt(username, password)
-            return token.toJSON()
-        } catch (error) {
-            if (error.code === 'E_INVALID_AUTH_UID' || error.code === 'E_INVALID_AUTH_PASSWORD')
-                return response.unauthorized({ error: 'Credenciais inválidas.' })
-            
-            return response.internalServerError({ error: 'Ocorreu um problema ao fazer login', details: error })
-        }
+        const token = await auth.use('api').attempt(username, password)
+        return token.toJSON()
     }
 
     public async logout({ auth, response }: HttpContextContract) {
-        try {
-            await auth.use('api').logout()
-            return response.ok('Usuário deslogou.')
-        } catch (error) {            
-            return response.status(500).send({ error })
-        }
+        await auth.use('api').logout()
+        return response.ok('Usuário deslogou.')
     }
 }
