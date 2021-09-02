f4b2be3a90ee2ebee722061d43841b29346545e7
Pedro Lopes <pedrolopes.info@gmail.com>
2021-09-02 15:45:17 -0300

    fix

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,19 +1,17 @@
 import Factory from '@ioc:Adonis/Lucid/Factory'
 import User from 'App/Models/Main/User'
 
-export const UserFactory = Factory
-  .define(User, ({ faker }) => {
-    return {
-        first_name: faker.name.firstName(),
-        last_name: faker.name.lastName(),
-        email: faker.internet.email(),
-        genderId: 1,
-        birth_date: faker.date.past(20),
-        cpf: faker.random.number(11).toString(),
-        phone_number: faker.phone.phoneNumber('(00)00000-0000'),
-        pending_approval: false,
-        approved: true,
-        active: true
-    }
-  })
-.build()
+export const UserFactory = Factory.define(User, ({ faker }) => {
+  return {
+    first_name: faker.name.firstName(),
+    last_name: faker.name.lastName(),
+    email: faker.internet.email(),
+    genderId: 1,
+    birth_date: faker.date.past(20),
+    cpf: faker.datatype.number(11).toString(),
+    phone_number: faker.phone.phoneNumber('(00)00000-0000'),
+    pending_approval: false,
+    approved: true,
+    active: true,
+  }
+}).build()
